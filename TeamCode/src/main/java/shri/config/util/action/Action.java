a6287def874273caab3d5e72e50b63aa30f83ff3
Sriyan <sriyanjuvvadi@gmail.com>
2025-02-23 17:41:05 -0500

    can revert later

--- TeamCode/src/main/java/shri/config/util/action/Action.java
+++
@@ -1,9 +0,0 @@
-package shri.config.util.action;
-import com.acmerobotics.dashboard.canvas.Canvas;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-
-public interface Action {
-    boolean run(TelemetryPacket p);
-
-    default void preview(Canvas fieldOverlay) {}
-}
